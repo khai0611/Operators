9eccfb1749428c7401180d604e8a173b1727831e
hqk0611 <khaihq19@uef.edu.vn>
2023-11-02 10:31:54 +0700

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Toán tử toán học
+            //int i = 15;
+            //int j = 5;
+            //int a = 10;
+            //int b = 3;
+
+            //Console.WriteLine(i + j);
+            //Console.WriteLine(i - j);
+            //Console.WriteLine(i * j);
+            //Console.WriteLine(i / j);
+            //Console.Write(Environment.NewLine);
+
+            //Console.WriteLine(a % b);
+
+            //Console.Write(Environment.NewLine);
+            //Console.WriteLine(i++); // Sử dụng giá trị i để in ra rồi mới tăng i
+            //Console.WriteLine(++j); // Tăng j lên rồi mới in giá trị j ra màn hình
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Toán tử quan hệ
+            //int y = 100;
+            //int z = 99;
+
+            //Console.WriteLine(y == z);
+            //Console.WriteLine(y != z);
+            //Console.WriteLine(y > z);
+            //Console.WriteLine(y < z);
+            //Console.WriteLine(y >= z);
+            //Console.WriteLine(y <= z);
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Toán tử logic
+            //int m = 10;
+            //int n = 15;
+            //int o = 5;
+
+            //Console.WriteLine(m < n && m > o);
+            //Console.WriteLine(m + n > o || n - m < o);
+            //Console.WriteLine(!(n > o && m > o));
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Toán tử khởi tạo và gán
+            //int x = 10;
+
+            //Console.WriteLine(x += 5);
+            //Console.WriteLine(x -= 5);
+            //Console.WriteLine(x *= 5);
+            //Console.WriteLine(x /= 5);
+
+            //Console.ReadKey();
+            #endregion
+        }
+    }
+}
+
+    //Toán tử là gì?
+    /*
+     * - Là một công cụ để thao tác với dữ liệu
+     * - Là một ký hiệu dùng để đại diện cho một thao tác cụ thể được thực hiện trên dữ liệu
+     * Có 4 loại toán tử:
+     * - Toán tử toán học (+, -, *, /, %, ++, --)
+     * - Toán tử quan hệ (==, !=, >, <, >=, <=)
+     * - Toán tử logic (&&, ||, !)
+     * - Toán tử khởi tạo và gán (=, +=, -=, *=, /=, %=)
+     */
